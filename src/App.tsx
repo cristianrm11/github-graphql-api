7368b1688e2bfeaf95dc77c4120d2ffede2a2871
cretana <cristianrm11@gmail.com>
2023-05-01 19:52:34 -0600

    Add some interfaces and queries to search repositories

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import { useQuery, gql } from '@apollo/client';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Repository {
+  nameWithOwner: string;
+  description: string;
+  url: string;
 }
 
-export default App
+interface SearchData {
+  search: {
+    nodes: Repository[];
+  };
+}
+
+interface SearchVars {
+  query: string;
+}
+
+const GET_REPOSITORIES = gql`
+  query GetRepositories($query: String!) {
+    search(query: $query, type: REPOSITORY, first: 10) {
+      nodes {
+        ... on Repository {
+          nameWithOwner
+          description
+          url
+        }
+      }
+    }
+  }
+`;
+
+function App() {
+  const [query, setQuery] = useState<string>('');
+  const { loading, error, data } = useQuery<SearchData, SearchVars>(GET_REPOSITORIES, {
+    variables: { query }
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <div>
+      <label>Type the name of the repository</label>
+      <input
+        type="text"
+        value={query}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+          setQuery(event.target.value)
+        }
+      />
+      <ul>
+        {data?.search.nodes.map((repo: Repository) => (
+          <li key={repo.url}>
+            <a href={repo.url}>{repo.nameWithOwner}</a>: {repo.description}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `Bearer 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`
+  },
+  cache: new InMemoryCache()
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
 )
